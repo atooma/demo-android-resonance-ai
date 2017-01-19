73be1e359f2d87638be706286bc9aafca9c425a9
Federico Di Pierro <nierro92@gmail.com>
2017-01-19 15:37:56 +0100

    dropped menu support in main activity

--- app/src/main/java/com/resonance_ai/sdk/sampleapp/MainActivity.java
+++ app/src/main/java/com/resonance_ai/sdk/sampleapp/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -83,14 +82,6 @@ public class MainActivity extends ResonanceActivity {
     @Override
     protected int getLocationPermissionCode() {
         return 900;
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
